de14433f1042bcce788abb6a90e43aa2659498b0
doraly <doraly9110@hotmail.com>
2022-04-10 12:22:25 -0500

    adding functionality to the form

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,25 +1,35 @@
-import React from 'react'
 
-const Contact = () => {
-  return (
-    <div name='contact' className='w-full h-full  bg-[#B1D7C8]  flex justify-center items-center p-2 '>
-        <form method='' action="" className='flex flex-col max-w-[600px] w-full'>
-            <div className='pb-8'>
-                <p className="text-4xl font-bold text-[#023047]">Contact</p>
-                
-            </div>
-            <input className=' shadow-sm border-slate-300  focus:outline-none focus:border-[#C39317] focus:ring-[#C39317] block w-full rounded-md sm:text-sm focus:ring-1 bg-[#fff] p-2 ' type="text" placeholder='Name' name='name' />
-            <input className='my-4 p-2 bg-[#fff]' type="email" placeholder='Email' name='email' />
-            <textarea className='bg-[#fff] p-2' name="message" rows="10" placeholder='Message'></textarea>
-            <button className=" text-white border-2 px-4 py-3 my-8 mx-auto flex items-center bg-[#023047] hover:bg-amber-500">
-            LET'S COLLABORATE
-           </button>
-           
-        
-        
-        </form>
-    </div>
-  )
+import React from 'react'
+import emailjs from 'emailjs-com';
+export default class Contact extends React.Component {
+  render() {
+      function enviarEmail(e){
+          e.preventDefault();
+
+          emailjs.sendForm('service_sgoyhbs', 'template_hxe2mkl', e.target, 'KI-dunL_Ng_VxiRSm')
+          .then(function(response) {
+             console.log('SUCCESS!', response.status, response.text);
+          }, function(error) {
+             console.log('FAILED...', error);
+          });
+      }
+
+
+return (
+<div name='contact' className='w-full h-full bg-[#B1D7C8] flex justify-center items-center p-2 '>
+<form id="myForm" onSubmit={enviarEmail} className='flex flex-col max-w-[600px] w-full'>
+<div className='pb-8'>
+<p className="text-4xl font-bold text-[#023047]">Contact</p>
+</div>
+<input className=' shadow-sm border-slate-300 focus:outline-none focus:border-[#C39317] focus:ring-[#C39317] block w-full rounded-md sm:text-sm focus:ring-1 bg-[#fff] p-2 ' type="text" placeholder='Name' name='name' />
+<input className='my-4 p-2 bg-[#fff]' type="email" placeholder='Email' name='email' />
+<textarea className='bg-[#fff] p-2' name="message" rows="10" placeholder='Message'></textarea>
+<button className=" text-white border-2 px-4 py-3 my-8 mx-auto flex items-center bg-[#023047] hover:bg-amber-500">
+LET'S COLLABORATE
+</button>
+</form>
+</div>
+)
 }
 
-export default Contact
+}
