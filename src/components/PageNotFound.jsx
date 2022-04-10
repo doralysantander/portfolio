27e2087dcac921177b053886cc61226c083fef6b
doraly <doraly9110@hotmail.com>
2022-04-10 00:15:38 -0500

    adding path not found error 404

--- src/components/PageNotFound.jsx
+++ src/components/PageNotFound.jsx
@@ -5,6 +5,7 @@ const PageNotFound = () => {
     <div className=" font-bold text-[#023047] flex flex-col justify-center items-center w-full h-screen ">
         <h1 className=" ">404</h1>
         <h2>Page Not Found</h2>
+        
 
     </div>
   )
