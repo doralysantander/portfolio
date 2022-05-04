0380d1db68f6e67ff08973c5bb839e1843d21be9
doraly <doraly9110@hotmail.com>
2022-05-04 11:49:35 -0500

    saving changes before implementing animation

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,3 +1,4 @@
+// antes de implementar motion
 import React from 'react'
 import { HiArrowNarrowRight } from 'react-icons/hi';
 //import {Link } from "react-router-dom";
@@ -8,7 +9,7 @@ const Home = () => {
     <div name="home" className = "w-full h-screen bg-[#023047]">
        <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full text-slate-100 '>
             <p className="text-white lg:text-2xl sm:text-3xl">Hi, my name is</p>
-            <h1 className=" lg:text-5xl sm:text-4xl font-bold">DORALY SANTANDER CHAMORRO</h1>
+            <h1  animate className=" lg:text-5xl sm:text-4xl font-bold">DORALY SANTANDER CHAMORRO</h1>
             <h2 className="text-4xl sm:text-3xl font-bold text-l">I'M A DEVELOPER</h2>
             <div className="" >
             <a href= {HV} download={'HV-DORALY-SANTANDER-CHAMORRO'}>
